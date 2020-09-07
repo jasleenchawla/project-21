9fed8bf57a74b40a5138cf0cb9e88efe65ae440e
jasleenchawla <69804595+jasleenchawla@users.noreply.github.com>
2020-09-07 22:20:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 var bullet,wall;
 var speed,weight;
 var bullet,thickness;
-thickness = random(22,83);
 
 function setup() {
   speed = random(223,321);
   weight = random(30,52);
   bullet = createSprite(50,200,50,50);
   wall = createSprite(1200, 200, thickness, height/2);
-  wall.shapeColor(80,80,80);
+  wall.shapeColor=color(80,80,80);
   bullet.velocityX = speed;
+  thickness = random(22,83);
+
 
   
     
@@ -17,31 +18,11 @@ function setup() {
   
   
   createCanvas(1600,400);
-  createSprite(400, 200, 50, 50);
+  
 }
 
 function draw() {
-  if (wall.x - bullet.x < (bullet.width + wall.width)/2)
-  {
-    bullet.velocityX = 0;
-    var deformation=0.5 * weight * speed * speed/22509;
-    if(deformation>180)
-    {
-      bullet.shapeColor=color(255,0,0);
-    }
-    if (deformation<180 && deformation>100)
-    {
-      bullet.shapeColor = color(230,230,0);
-
-    }
-    if (deformation<100)
-    {
-      bullet.shapeColor = color(0,255,0);
-    }
-
-
-  }
-
+  background (0); 
     if (hascollided(bullet,wall))
     {
       bullet.velocityX = 0;
@@ -52,15 +33,15 @@ function draw() {
           wall.shapeColor = color(255,0,0);
       }
 
-      ig (damage<10)
+      if (damage<10)
       {
         wall.shapeColor = color (0,255,0);
       }
     }
     
     
-  hascollided();
-  background (0);  
+  
+   
   drawSprites();
 }
 
